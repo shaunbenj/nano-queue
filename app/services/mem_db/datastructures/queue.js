2a9b466ea43bdd2653793a44620e1b90f4a54082
Shaun Benjamin <shaun.benj@gmail.com>
2024-12-29 19:29:04 -0800

    Add undo capability to datastructures

--- app/services/mem_db/datastructures/queue.js
+++ app/services/mem_db/datastructures/queue.js
@@ -1,7 +1,10 @@
+import Command from "../../../../lib/command/command.js";
 import DoublyLinkedList from "../../../../lib/doubly_linked_list.js";
+import Base from "./base.js";
 
-export default class Queue {
+export default class Queue extends Base {
   constructor(list = new DoublyLinkedList()) {
+    super();
     this.list = list;
   }
 
@@ -17,12 +20,20 @@ export default class Queue {
     this.list.insertHeadBatch(dataArray);
   }
 
+  leftPeek() {
+    return this.list.peekHead();
+  }
+
   leftPop() {
     return this.list.popHead();
   }
 
   leftBatchPop(n) {
     return this.list.popHeadBatch(n);
+  }
+
+  rightPeek() {
+    return this.list.peekTail();
   }
 
   rightPush(data) {
@@ -43,5 +54,39 @@ export default class Queue {
 
   rightBatchPop(n) {
     return this.list.popTailBatch(n);
+  }
+
+  undoCommand(operation, ...args) {
+    if (operation == "leftPush") {
+      return new Command(this, "leftPop");
+    } else if (operation == "leftBatchPush") {
+      return new Command(this, "leftBatchPop", args[0].length);
+    } else if (operation == "leftPeek") {
+      return Command.noop();
+    } else if (operation == "leftPop") {
+      return new Command(this, "leftPush", this.leftPeek());
+    } else if (operation == "leftBatchPop") {
+      const poppedValues = this.list.batchPeekHead(args[0]);
+
+      // Skipping deep copy as primitive types including strings are copy by
+      // value
+      return new Command(this, "leftBatchPush", poppedValues.reverse());
+    } else if (operation == "rightPeek") {
+      return Command.noop();
+    } else if (operation == "rightPush") {
+      return new Command(this, "rightPop");
+    } else if (operation == "rightBatchPush") {
+      return new Command(this, "rightBatchPop", args[0].length);
+    } else if (operation == "rightPop") {
+      return new Command(this, "rightPush", this.rightPeek());
+    } else if (operation == "rightBatchPop") {
+      const poppedValues = this.list.batchPeekTail(args[0]);
+
+      // Skipping deep copy as primitive types including strings are copy by
+      // value
+      return new Command(this, "rightBatchPush", poppedValues.reverse());
+    } else {
+      throw new Error(`Unrecognized operation ${operation} for Queue`);
+    }
   }
 }
--- test/app/services/mem_db/datastructures/queue_test.js
+++ test/app/services/mem_db/datastructures/queue_test.js
@@ -21,4 +21,45 @@ describe("Queue", () => {
     queue.rightBatchPush([1, 2, 3]);
     assert.deepStrictEqual(queue.leftBatchPop(3), [1, 2, 3]);
   });
+
+  it("generates undo commands", () => {
+    const queue = new Queue();
+    queue.leftPush(1);
+    queue.rightPush(10);
+
+    // Runs an operation and its corresponding undo operation. Verfies that the
+    // undo restores it back to the original state.
+    const runTest = (operation, args) => {
+      const undo = queue.undoCommand(operation, ...args);
+
+      queue[operation](...args);
+
+      undo.execute();
+
+      assert.strictEqual(queue.leftPeek(), 1);
+      assert.strictEqual(queue.rightPeek(), 10);
+    };
+
+    const tests = [
+      ["leftPush", [2]],
+      ["leftBatchPush", [[1, 2, 3]]],
+      ["leftPeek", []],
+      ["leftPop", []],
+      ["leftBatchPop", [3]],
+      ["rightPeek", []],
+      ["rightPush", [2]],
+      ["rightBatchPush", [[2, 3, 4]]],
+      ["rightPop", [1]],
+      ["rightBatchPop", [3]],
+    ];
+
+    tests.forEach((test) => {
+      runTest(...test);
+    });
+
+    // Raise error for an unrecognzed command
+    assert.throws(() => {
+      queue.undoCommand("invalid");
+    }, new Error("Unrecognized operation invalid for Queue"));
+  });
 });
